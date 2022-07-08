44089b0d8bb2e854053a816daf7a5d6bff630710
ToghrulShukurlu <106272453+ToghrulShukurlu@users.noreply.github.com>
2022-07-08 21:04:14 +0400

    Update main.js

--- task-9/main.js
+++ task-9/main.js
@@ -1,6 +1,9 @@
-// Use these variables in your code
-
-let s = "This is a cat sitting on a table"
-let c = "t"
-
-// Write code here. 
+const str = "This is a cat sitting on a table"
+const str1  = "t"
+const arr = str.split("t")
+count = 0;
+for (let i = 0; i < arr.length; i++) {
+count++
+    
+}
+console.log(count);e. 
